b49814f4fcd866e1ee3da210b48f923567246e95
ingrid <fermingrid@gmail.com>
2018-05-04 13:22:03 +0200

    idempotency test

--- FintechPlatformTests/CashInApiTest.swift
+++ FintechPlatformTests/CashInApiTest.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+import XCTest
+@testable import FintechPlatform
+
+class CashInApiTest: XCTestCase {
+    
+    let fintechAPI = FintechPlatformAPI.sharedInstance
+    
+    override func setUp() {
+        
+    }
+    
+    func testIdempotencyCashIn() {
+        let cashInAPI = fintechAPI.getCashInAPI(hostName: "10.0.0.9:9000")
+//        cashInAPI.cashIn(token: <#T##String#>, ownerId: <#T##String#>, accountId: <#T##String#>, accountType: <#T##String#>, tenantId: <#T##String#>, cardId: <#T##String#>, amount: <#T##Money#>, idempotency: <#T##String?#>, completion: <#T##(CashInResponse?, Error?) -> Void#>)
+    }
+}
