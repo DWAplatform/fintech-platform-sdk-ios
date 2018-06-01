695c3a602c508b6d92d25faf063af22dd6eb34e6
ingrid <fermingrid@gmail.com>
2018-06-01 09:40:21 +0200

    init kyc required

---
+++ FintechPlatform/fintech/kycAPI.swift
@@ -0,0 +1,69 @@
+//
+//  kycAPI.swift
+//  FintechPlatform
+//
+//  Created by ingrid on 31/05/18.
+//  Copyright © 2018 Fintech Platform. All rights reserved.
+//
+
+import Foundation
+class kycAPI {
+    lazy var session: SessionProtocol = URLSession.shared
+    
+    private let hostName: String
+    
+    public init(hostName: String) {
+        self.hostName = hostName
+    }
+    
+    func kycRequired(token: String, tenantId: String, ownerId: String, accountId: String, accountType: String, completion: @escaping ([DocType]?, Error?) -> Void) {
+        let path = NetHelper.getPath(from: accountType)
+        do {
+            guard let url = URL(string: hostName + "/rest/v1/fintech/tenants/\(tenantId)/\(path)/\(ownerId)/accounts/\(accountId)/kycRequiredDocuments")
+                else { fatalError() }
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.addBearerAuthorizationToken(token: token)
+            
+            session.dataTask(with: request) { (data, response, error) in
+                if let error = error { completion(nil, error); return }
+                guard let data = data else {
+                    completion(nil, WebserviceError.DataEmptyError)
+                    return
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    completion(nil, WebserviceError.NoHTTPURLResponse)
+                    return
+                }
+                
+                if (httpResponse.statusCode != 200) {
+                    completion(nil, NetHelper.createRequestError(data: data, error: error))
+                    return
+                }
+                
+                do {
+                    let reply = try JSONSerialization.jsonObject(
+                        with: data,
+                        options: []) as? [String:Any]
+                    var kycRequired = [DocType]()
+                    if let docType = reply?["docType"] as? [[String]] {
+                        for types in docType {
+                            for type in types{
+                                
+                            }
+                        }
+                    }
+                    
+                    completion(kycRequired, nil)
+                } catch {
+                    completion(nil, WebserviceError.NOJSONReply)
+                }
+                
+                }.resume()
+        } catch let error {
+            completion(nil, error)
+        }
+    }
+}
---
+++ FintechPlatform/models/DocType.swift
@@ -0,0 +1,14 @@
+//
+//  DocType.swift
+//  FintechPlatform
+//
+//  Created by ingrid on 31/05/18.
+//  Copyright © 2018 Fintech Platform. All rights reserved.
+//
+
+import Foundation
+enum DocType {
+    case IDENTITY_CARD
+    case PASSPORT
+    case DRIVING_LICENCE
+}
