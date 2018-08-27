81eba7898a8ef485da38533ffb07de2837ec47b6
ingrid <fermingrid@gmail.com>
2018-08-27 14:53:46 +0100

    add Account refactored constructors

--- FintechPlatform/models/Account.swift
+++ FintechPlatform/models/Account.swift
@@ -29,10 +29,10 @@ public class Account {
         return Account(tenantId: user.tenantId, accountType: AccountType.PERSONAL, ownerId: user.userId, accountId: accountId)
     }
     
-//    public static func businessAccount(enterprise: Enterprise,
-  //                                     accountId: UUID) -> Account {
-    //    return Account(tenantId: user.tenantId, accountType: AccountType.BUSINESS, ownerId: enterprise.enterpriseId, accountId: accountId)
-    //}
+    public static func businessAccount(enterprise: Enterprise,
+                                       accountId: UUID) -> Account {
+        return Account(tenantId: enterprise.tenantId, accountType: AccountType.BUSINESS, ownerId: enterprise.enterpriseId, accountId: accountId)
+    }
 }
 
 public struct User {
@@ -50,18 +50,13 @@ public enum AccountType {
     case PERSONAL
     case BUSINESS
 }
-/*data class Account internal constructor (val tenantId: UUID,
-val accountType: AccountType,
-val ownerId: UUID,
-val accountId: UUID) {
+
+public struct Enterprise {
+    public let enterpriseId: UUID
+    public let tenantId: UUID
     
-    companion object {
-        
-        fun personalAccount(user: User,
-                            accountId: UUID): Account = Account(user.tenantId, AccountType.PERSONAL, user.userId, accountId)
-        
-        fun businessAccount(enterprise: Enterprise,
-        accountId: UUID): Account = Account(enterprise.tenantId, AccountType.BUSINESS, enterprise.enterpriseId, accountId)
+    public init(enterpriseId: UUID, tenantId: UUID) {
+        self.enterpriseId = enterpriseId
+        self.tenantId = tenantId
     }
 }
-*/
